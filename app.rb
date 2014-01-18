dcfd6b39e82b31435ef60ef7e44fdcf0ad10993d
melodicodyssey84 <stevedbauer@gmail.com>
2014-01-17 16:59:01 -0800

    refactored 5pm friday

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
+def omdb_typh_get(type,movie)
+	Typhoeus.get("omdbapi.com", :params => {type => movie})
+end
+
 # build homepage
 get "/" do
 
@@ -10,23 +14,18 @@ erb :search
 
 end
 
+
 post "/results" do
+	#establish string to search for:
+	to_search = params[:movie] # == "Star Wars"
 
-	to_search = params[:movie] # == "Cars"
-
-	search_result = Typhoeus.get("www.omdbapi.com/",:params => {:s => to_search})
-	@movie_list = JSON.parse(search_result.body)['Search'].sort_by {|movie| movie['Year']}
-
-	#add the movie posters to the hashes:
-	@movie_list.each do |movie|
-		poster_search = Typhoeus.get("www.omdbapi.com/i=#{movie['imdbID']}")
-		# Categories:
-		# Title, Year, Rated, Released, Runtime, Genre, Director, Writer, Actors, Plot, Language, Country, Awards, Poster, Metascore, imdbRating, imdbVotes, imdbID, Type, Response
-		info = JSON.parse(search_result.body)
-		movie['Poster'] = info['Poster']
-		movie['Plot'] = info['Plot']
+	#query omdb, parse results, and do refined search with more info:
+	result = omdb_typh_get(:s,to_search)
+	@movie_list = JSON.parse(result.body)['Search'].map! do |movie|
+		refined_search = omdb_typh_get(:i,movie['imdbID'])
+		info = JSON.parse(refined_search.body)
+		movie = info
 	end
-
 
 	erb :test_results
 
