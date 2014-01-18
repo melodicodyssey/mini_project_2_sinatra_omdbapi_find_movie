8098d6f0e1d71b30fb6f32363e1c9da48db064f2
melodicodyssey84 <stevedbauer@gmail.com>
2014-01-17 18:34:14 -0800

    further refactoring

--- app.rb
+++ app.rb
@@ -3,30 +3,18 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
-def omdb_typh_get(type,movie)
-	Typhoeus.get("omdbapi.com", :params => {type => movie})
+def parser(type,movie)
+	parser = JSON.parse(Typhoeus.get("omdbapi.com", :params => {type => movie}).body)
 end
 
-# build homepage
 get "/" do
-
 erb :search
-
 end
-
 
 post "/results" do
-	#establish string to search for:
-	to_search = params[:movie] # == "Star Wars"
-
-	#query omdb, parse results, and do refined search with more info:
-	result = omdb_typh_get(:s,to_search)
-	@movie_list = JSON.parse(result.body)['Search'].map! do |movie|
-		refined_search = omdb_typh_get(:i,movie['imdbID'])
-		info = JSON.parse(refined_search.body)
-		movie = info
+	redirect "/" if params[:movie] == ""
+	@movies = parser(:s,params[:movie])['Search'].map! do |movie| 
+		movie = parser(:i,movie['imdbID'])
 	end
-
 	erb :test_results
-
 end
